c6976da889d4ba5246c1c9a507f9869bd469d1ca
Curtis Campbell <79437242+chortis@users.noreply.github.com>
2024-11-21 09:34:59 -0800

    Initialize a little test app

---
+++ component-demo/src/Components.tsx
@@ -0,0 +1,11 @@
+function Components() {
+  return (
+    <div className="components">
+      <h1>Storybook</h1>
+      <p>Imagine this is an amazing Storybook app</p>
+      <div className="component-list"></div>
+    </div>
+  );
+}
+
+export { Components };
