f54ddab21bc14b542444de75621641fe54af27e1
Curtis Campbell <79437242+chortis@users.noreply.github.com>
2025-02-14 09:13:00 -0800

    Ad Button API

---
+++ component-demo/src/components/Button/Button.types.ts
@@ -0,0 +1,10 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+
+/** This is the button component */
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  "data-testid": string;
+  size: "sm" | "md" | "large";
+};
